defe3593d038e40b11378b02ae1f25648e0ebab7
kirkarr <kirkanar231@gmail.com>
2024-12-09 16:49:02 +0300

    Added wallpaper
    Added main page, style and script for it. Also added minecraft font and Wallpaper Engine project file.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# newyearwallpaper
-Wallpaper for Wallpaper Engine with New Year countdown.
+# New Year Wallpaper
+Desktop wallpaper created using Wallpaper Engine! That unique wallpaper display the current time and countdown to the New Year, creating a celebratory atmosphere.
+
+## Installation
+1. Ensure you have the latest version of Wallpaper Engine installed.
+2. Download the wallpapers from this repository.
+3. Import the wallpapers into Wallpaper Engine through the "Import Wallpaper" interface.
+4. Set the wallpaper as active to enjoy the winter ambiance!
+
+## Demo
+You can see demo on [codepen](https://pages.github.com/). (Without minecraft font)
+
+## Contributing
+If you have ideas for improvements or want to suggest new features, feel free to create pull requests or open issues. Your feedback will help enhance the project!
+
---
+++ script.js
@@ -0,0 +1,281 @@
+let symbols = JSON.parse('{"1": [[148, 274], [163, 260], [175, 250], [189, 241], [200, 229], [211, 217], [223, 206], [236, 196], [250, 187], [266, 172], [280, 156], [290, 144], [298, 130], [306, 116], [314, 101], [322, 87], [329, 132], [334, 157], [338, 176], [339, 196], [339, 211], [338, 232], [338, 248], [336, 269], [335, 284], [335, 306], [335, 324], [336, 345], [336, 363], [336, 381], [336, 398], [337, 414], [338, 430], [339, 448], [341, 465], [341, 480]], "2": [[180, 212], [186, 193], [193, 178], [204, 158], [215, 140], [228, 127], [247, 115], [265, 106], [280, 99], [297, 92], [317, 87], [332, 83], [348, 80], [364, 79], [383, 79], [399, 83], [413, 95], [424, 108], [434, 121], [443, 133], [445, 151], [445, 169], [441, 187], [435, 202], [425, 214], [412, 227], [400, 238], [387, 250], [371, 260], [356, 267], [341, 275], [328, 283], [314, 290], [299, 297], [281, 307], [267, 315], [248, 325], [233, 331], [220, 339], [206, 349], [191, 360], [177, 367], [162, 377], [149, 386], [137, 396], [128, 408], [120, 421], [114, 435], [130, 447], [149, 449], [164, 449], [182, 449], [199, 447], [216, 446], [231, 446], [248, 446], [264, 447], [282, 448], [304, 448], [328, 448], [351, 448], [369, 448], [389, 449], [406, 449], [424, 448], [441, 448], [457, 449], [473, 450]], "3": [[186, 163], [200, 149], [214, 140], [230, 134], [249, 128], [272, 123], [291, 116], [314, 109], [331, 106], [348, 105], [366, 104], [385, 112], [397, 123], [408, 135], [415, 150], [415, 166], [415, 185], [416, 206], [416, 221], [411, 236], [403, 249], [393, 263], [379, 272], [361, 279], [347, 286], [327, 292], [306, 297], [290, 300], [275, 301], [256, 303], [240, 307], [225, 313], [322, 307], [337, 304], [355, 300], [373, 305], [389, 312], [398, 326], [406, 345], [414, 364], [416, 379], [413, 398], [408, 413], [399, 430], [389, 443], [374, 462], [361, 475], [346, 487], [330, 495], [314, 500], [299, 504], [279, 506], [263, 506], [244, 506], [229, 505], [214, 502], [199, 493]], "4": [[171, 102], [171, 118], [171, 135], [170, 150], [168, 172], [166, 190], [163, 206], [161, 225], [158, 243], [156, 258], [156, 273], [174, 272], [190, 272], [205, 272], [221, 272], [239, 271], [256, 271], [273, 271], [288, 273], [304, 275], [319, 275], [334, 275], [351, 273], [365, 267], [376, 255], [376, 237], [375, 222], [375, 202], [376, 185], [375, 166], [371, 151], [368, 135], [369, 119], [373, 104], [389, 196], [390, 216], [391, 234], [388, 264], [385, 279], [381, 298], [378, 317], [378, 333], [378, 348], [376, 366], [376, 381], [376, 396], [377, 412], [378, 427]], "5": [[377, 121], [358, 118], [334, 118], [315, 118], [298, 118], [281, 118], [265, 118], [248, 118], [233, 120], [218, 120], [202, 118], [184, 116], [166, 115], [152, 124], [151, 141], [150, 158], [148, 174], [146, 189], [144, 206], [142, 221], [140, 237], [136, 254], [134, 270], [133, 285], [152, 275], [164, 262], [176, 252], [191, 238], [206, 231], [227, 225], [244, 221], [264, 217], [282, 215], [297, 215], [314, 215], [332, 221], [347, 228], [362, 244], [373, 256], [382, 270], [389, 285], [395, 302], [398, 317], [400, 335], [396, 350], [392, 366], [384, 379], [374, 393], [360, 402], [344, 411], [328, 418], [314, 424], [298, 430], [281, 432], [264, 433], [246, 433], [227, 436], [212, 437], [194, 434], [180, 425], [163, 421], [148, 414]], "6": [[298, 92], [282, 95], [270, 106], [256, 123], [248, 139], [243, 154], [236, 171], [230, 185], [224, 203], [220, 218], [214, 233], [208, 255], [203, 271], [200, 287], [198, 302], [196, 318], [194, 340], [193, 355], [193, 370], [194, 385], [207, 396], [224, 405], [238, 412], [258, 419], [275, 422], [292, 420], [307, 416], [322, 407], [339, 398], [357, 387], [368, 376], [376, 363], [382, 348], [383, 332], [376, 317], [364, 298], [353, 281], [339, 268], [322, 259], [304, 251], [288, 242], [276, 233], [263, 224], [248, 219]], "7": [[189, 106], [205, 104], [221, 102], [241, 100], [267, 98], [293, 98], [309, 98], [326, 97], [343, 96], [359, 96], [380, 96], [400, 96], [415, 96], [430, 95], [419, 113], [409, 128], [400, 140], [392, 153], [376, 169], [364, 185], [351, 200], [342, 214], [331, 226], [322, 238], [313, 252], [303, 264], [294, 276], [285, 291], [276, 303], [265, 318], [255, 330], [246, 342], [237, 354], [225, 368], [216, 380], [206, 393], [213, 228], [230, 228], [248, 228], [265, 225], [282, 225], [297, 223], [312, 223], [354, 224], [369, 224], [384, 228], [399, 232], [416, 235], [431, 236]], "8": [[295, 90], [278, 84], [260, 83], [246, 89], [234, 103], [225, 115], [217, 129], [213, 144], [212, 159], [211, 175], [214, 193], [216, 209], [225, 224], [239, 231], [253, 239], [268, 244], [283, 250], [296, 258], [311, 267], [321, 280], [326, 297], [329, 312], [330, 330], [325, 345], [317, 362], [311, 376], [300, 388], [285, 395], [268, 396], [249, 399], [233, 400], [217, 401], [201, 400], [185, 395], [173, 386], [160, 376], [152, 362], [148, 346], [147, 331], [148, 315], [151, 299], [156, 284], [166, 272], [179, 263], [197, 257], [212, 251], [227, 248], [295, 237], [311, 235], [327, 232], [343, 228], [355, 219], [365, 206], [374, 193], [382, 180], [389, 166], [393, 150], [391, 135], [378, 126], [363, 118], [349, 109], [333, 101], [319, 95]], "9": [[247, 430], [259, 439], [276, 441], [292, 444], [307, 443], [324, 432], [335, 421], [347, 405], [356, 388], [363, 367], [368, 351], [371, 334], [374, 316], [377, 296], [379, 275], [381, 258], [382, 243], [384, 225], [387, 208], [389, 189], [390, 170], [390, 151], [389, 135], [385, 119], [377, 104], [368, 88], [359, 76], [345, 69], [330, 68], [315, 65], [300, 65], [286, 73], [272, 86], [262, 101], [253, 115], [250, 131], [249, 147], [253, 164], [256, 179], [265, 192], [276, 205], [294, 214], [310, 221], [325, 224], [341, 226], [356, 223]], "0": [[337, 111], [329, 97], [312, 83], [296, 76], [280, 68], [265, 62], [250, 62], [236, 70], [226, 82], [215, 96], [205, 109], [196, 121], [189, 135], [181, 154], [178, 169], [176, 185], [174, 202], [173, 221], [171, 240], [171, 257], [170, 273], [173, 292], [174, 312], [176, 328], [182, 347], [187, 366], [193, 382], [203, 400], [214, 412], [230, 422], [249, 428], [267, 431], [284, 432], [301, 426], [319, 414], [332, 403], [345, 392], [354, 378], [357, 362], [363, 343], [366, 327], [369, 307], [372, 291], [374, 276], [376, 257], [378, 236], [380, 221], [378, 203], [376, 185], [372, 166], [367, 150], [356, 138], [345, 127]], ":": [[261, 142], [255, 128], [270, 124], [260, 311], [255, 295], [270, 296], [258, 326], [245, 316], [276, 312], [240, 140], [233, 124], [251, 113], [277, 143], [283, 116], [237, 107], [247, 94], [265, 98], [293, 100], [304, 112], [306, 132], [304, 148], [237, 299], [288, 327], [290, 342], [272, 347], [253, 348], [242, 331]]}')
+function HSVtoRGB(h, s, v) {
+	var r, g, b, i, f, p, q, t;
+
+	if (arguments.length === 1) {
+		s = h.s, v = h.v, h = h.h;
+	}
+
+	i = Math.floor(h * 6);
+
+	f = h * 6 - i;
+
+	p = v * (1 - s);
+
+	q = v * (1 - f * s);
+
+	t = v * (1 - (1 - f) * s);
+
+	switch (i % 6) {
+		case 0:
+			r = v, g = t, b = p;
+		break;
+
+		case 1:
+			r = q, g = v, b = p;
+		break;
+
+		case 2:
+			r = p, g = v, b = t;
+		break;
+
+		case 4:
+			r = t, g = p, b = v;
+		break;
+
+		case 3:
+			r = p, g = q, b = v;
+		break;
+
+		case 5:
+			r = v, g = p, b = q;
+		break;
+	}
+
+	return {
+		r: Math.round(r * 255),
+		g: Math.round(g * 255),
+		b: Math.round(b * 255) 
+	};
+
+}
+
+let blockcounter = 0;
+function spawn(x, y, vx, vy, text, clr, lifetime) {
+  var pixels = document.querySelector("#pixels");
+  var block = {};
+  block.x = x;
+  block.y = y;
+  block.vx = vx;
+  block.vy = vy;
+  block.targetx = 0;
+  block.targety = 0;
+  block.size = size / 2 + getRandomInt(size / 2);
+  block.id = blockcounter;
+  block.obj = document.createElement("div");
+  block.obj.style.left = `${x}px`;
+  block.obj.style.top = `${y}px`;
+  block.obj.style.width = `${block.size}px`;
+  block.obj.style.height = `${block.size}px`;
+  block.obj.style.fontSize = `${block.size}px`;
+  block.obj.style.textshadow = `rgb(${clr.r}, ${clr.g}, ${clr.b})`;
+  block.obj.style.color = `rgb(${clr.r}, ${clr.g}, ${clr.b})`;
+  block.obj.className = "block";
+  if(text == " " || text == ""){text="JS"}
+  block.obj.innerHTML = text;
+  pixels.appendChild(block.obj);
+  blocks.push(block);
+  blockcounter++;
+}
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+
+let scw = window.innerWidth;
+
+let sch = window.innerHeight;
+
+let blocks = [];
+let size = 50;
+let target = 300;
+let limit = 550;
+
+let cur = 0;
+let anim = 1;
+
+let mousex = 0;
+let mousey = 0;
+let mousevelx = 0;
+let mousevely = 0;
+
+let movesectors = {};
+let denisty = 100;
+
+var progress = document.querySelector("#progress");
+progress.max = target
+
+function drawSymbol(id){
+	/* Clearing old particles*/
+	blocks.forEach((block, blockIndex) => {
+		if(block.targetx != 0){
+			block.targetx = 0;
+			block.targety = 0;
+			block.vx = getRandomInt(10) - 5;
+			block.vy = getRandomInt(10) - 5;
+		}
+	});
+	
+	/* Selecting new particles*/
+	symbol = symbols[id]
+	symbol.forEach((pos, posIndex) => {
+		if(posIndex < blockcounter){
+			blocks[posIndex].targetx = pos[0]
+			blocks[posIndex].targety = pos[1]
+			console.log(posIndex, pos);
+		}
+	});
+}
+function drawText(text){
+	/* Clearing old particles*/
+	blocks.forEach((block, blockIndex) => {
+		if(block.targetx != 0){
+			block.targetx = 0;
+			block.targety = 0;
+			block.vx = getRandomInt(10) - 5;
+			block.vy = getRandomInt(10) - 5;
+		}
+	});
+	let data = text.split("");
+	let used = 0;
+	let padding = 0;
+	/* Selecting new particles*/
+	data.forEach((sym, offset) => {
+		prepad = 0;
+		symbol = symbols[sym]
+		symbol.forEach((pos, posIndex) => {
+			if(posIndex + used < blocks.length){
+				if(pos[0] + padding > padding){ prepad = pos[0]}
+				blocks[posIndex + used].targetx = pos[0] + padding
+				blocks[posIndex + used].targety = pos[1]
+			}
+		});
+		used += symbol.length;
+		console.log(`${sym} took ${symbol.length}`);
+		if(sym==3 || sym==5){prepad+=100};
+		padding += prepad;
+	});
+}
+
+var counter = document.querySelector("#counter");
+var taken = document.querySelector("#taken");
+var fps = 0;
+let interval = setInterval(function () {
+	today = new Date();
+	millis = (timeend-today);
+	miltimer.innerHTML = Math.round(millis) + ' millis';
+	counter.innerHTML = `${blocks.length} particles`
+	if(cur<target){
+		blck = spawn(getRandomInt(scw),
+		getRandomInt(sch),
+		((getRandomInt(100) - 50) / 100) * 10,
+		((getRandomInt(100) - 50) / 100) * 10,
+		"*",
+		HSVtoRGB(0, 0, 0.25),
+		(1 + getRandomInt(4)) * 650000);
+		cur += 1;
+		progress.value = cur
+	}else{
+		took = 0;
+		progress.hidden = true;
+		progress.hidden = true;
+		blocks.forEach((block, blockIndex) => {
+			if(block.targetx != 0){
+				clr = HSVtoRGB(block.x / 300 + anim / 150, 1, 1);
+				block.obj.style.color = `rgb(${clr.r}, ${clr.g}, ${clr.b})`;
+				took += 1;
+				block.vx = (block.targetx - block.x) / 25
+				block.vy = (block.targety - block.y) / 25
+			}else{
+				clr = HSVtoRGB(0, 0, 0.75);
+				block.obj.style.color = `rgb(${clr.r}, ${clr.g}, ${clr.b})`;
+			}
+			block.vx += (scw / 2 - block.x) / (500 + getRandomInt(500))
+			if(block.vy < 5){
+				block.vy += 0.1
+			}
+			block.x += block.vx
+			block.y += block.vy
+			block.vx /= 1.001
+			block.vy /= 1.001
+			if(block.y > sch){
+				block.vy = 0
+				block.y = -15
+				block.x = getRandomInt(scw)
+			}
+			block.obj.style.top = `${block.y}px`;
+			block.obj.style.left = `${block.x}px`;
+		});
+		anim += 1;
+	}
+	fps += 1;
+	taken.innerHTML = `${took} used by art`
+}, 15);
+document.onkeypress = function (e) {
+	if(getRandomInt(100) > Math.max(15, (blocks.length - limit))){
+		key = e.keyCode
+		spawn(
+		 scw / 2,
+		 sch / 2,
+		 getRandomInt(30) - 15,
+		 getRandomInt(30) - 15,
+		 "*",
+		 {r:255, g:255, b:255},
+		 3000 + getRandomInt(10000)
+		);
+	};
+}
+document.onmousemove = function (e) {
+	mousevelx = e.clientX - mousex
+	mousevely = e.clientY - mousey
+	mousex = e.clientX;
+	mousey = e.clientY;
+}
+var frame = document.querySelector("#frame");
+var runtime = document.querySelector("#runtime");
+var clock = document.querySelector("#clock");
+var subclock = document.querySelector("#subclock");
+var timer = document.querySelector("#timer");
+var miltimer = document.querySelector("#mtimer");
+var starttime = new Date();
+var timeend = new Date();
+timeend = new Date(timeend.getYear()>1900?(timeend.getYear()+1):(timeend.getYear()+1901),0,1);
+starttime.setTime(starttime.getTime() + (3600000 * 3));
+setInterval(function () {
+	today = new Date();
+	millis = (timeend-today);
+	today = Math.floor(millis / 1000);
+	tsec=today%60; today=Math.floor(today/60); if(tsec<10)tsec='0'+tsec;
+	tmin=today%60; today=Math.floor(today/60); if(tmin<10)tmin='0'+tmin;
+	thour=today%24; today=Math.floor(today/24);
+	timestr=today +" days "+ thour+" hours "+tmin+" minutes "+tsec+" seconds";
+	timer.innerHTML=timestr;
+	scw = window.innerWidth;
+	sch = window.innerHeight;
+	var date = new Date();
+	var diff = new Date(date.getTime() - starttime.getTime());
+	runtime.innerHTML = `Runtime: ${diff.toTimeString().split(' ')[0]}`
+	clock.innerHTML = date.toTimeString().split(' ')[0]
+	if(mousex < 50 && mousey < 50){
+		text = date.toTimeString().split(' ')[0].split(":")
+		clck = text[0] + ":" + text[1]
+		drawText(clck);
+	}else{
+		drawText("")
+	}
+	frame.innerHTML = `${fps} FPS`;
+	fps = 0;
+	var datedata = date.toDateString().split(' ');
+	subclock.innerHTML = date.toLocaleString('default', { month: 'long' }) + " " + datedata[2] 
+	if(blocks.length < limit){
+		for(i = 1; i <= 5; i++){
+			blck = spawn(getRandomInt(scw),
+				getRandomInt(sch),
+				(getRandomInt(100) / 100) * 10,
+				(getRandomInt(100) / 100) * 10,
+				"*",
+				HSVtoRGB(0, 0, 0.25),
+				3000 + getRandomInt(50000));
+		}
+	}
+}, 1000)
